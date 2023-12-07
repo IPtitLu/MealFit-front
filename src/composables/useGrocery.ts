edb4c6f43ffc7503f0413e4063003ce4efcbd772
Douglas Barlow <d.barlow@laposte.net>
2023-12-07 18:13:30 +0800

    added useGrocery

---
+++ src/composables/useGrocery.ts
@@ -0,0 +1,60 @@
+import { ref } from 'vue';
+import axios from 'axios';
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+interface IGroceryItem {
+  ingredientId: string;
+  qty: number;
+  unit: string;
+  purchaseDate?: Date;
+}
+
+interface IUserGroceryList {
+  userId: string;
+  items: IGroceryItem[];
+}
+
+export const useGrocery = () => {
+  const groceryList = ref<IUserGroceryList>({ userId: '', items: [] });
+
+  const addGroceryItem = async (userId: string, id: string, qty: number, unit: string) => {
+    try {
+      const response = await axios.post<IUserGroceryList>(`${baseUrl}/add`, {
+        userId,
+        id,
+        qty,
+        unit,
+      });
+      groceryList.value = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getGroceryList = async (userId: string) => {
+    try {
+      const response = await axios.get<IUserGroceryList>(`${baseUrl}/${userId}`);
+      groceryList.value = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateGroceryItem = async (userId: string, itemId: string, updatedData: { qty: number; unit: string }) => {
+    try {
+      const response = await axios.put<IUserGroceryList>(`${baseUrl}/update/${itemId}`, updatedData);
+      groceryList.value = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  return {
+    groceryList,
+    addGroceryItem,
+    getGroceryList,
+    updateGroceryItem,
+  };
+}
